5c142af372bf5e248ff9a300baeaf29aeaac4b22
alberico <asoares@gdmseeds.com>
2024-10-04 15:59:15 -0300

    Revert "Refactor login page and update main.dart"
    This reverts commit 155ff04619f67b7a7c11fa322460c4ec7d46309f.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:develop_design_system/Views/Signup/signup.dart';
+import 'package:develop_design_system/Views/Login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       color: Colors.white,
-      home: SignUpPage()
+      home: LoginPage()
       );
   }
 }
